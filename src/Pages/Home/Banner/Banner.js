2fbda77345ca6a5d04822f6f8a0da1b384b91209
arifphero <ahammadarif232@gmail.com>
2021-12-01 14:53:30 +0600

    banner part ready

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,9 +1,45 @@
 import React from "react";
+import "./Banner.css";
+import Me from "../../../Images/img-4.png";
 
 const Banner = () => {
   return (
-    <div>
-      <h2>this is Banner</h2>
+    <div className="i ">
+      <div className="i-left">
+        <div className="i-left-wrapper">
+          <h2 className="i-intro">Hello, My name is </h2>
+          <h1 className="i-name">Arif Ahammad</h1>
+          <div className="i-title">
+            <div className="i-title-wrapper">
+              <div className="i-title-item">Blogger</div>
+              <div className="i-title-item"> Photographer</div>
+              <div className="i-title-item">Junior Dev Developer </div>
+              <div className="i-title-item">React Developer</div>
+              <div className="i-title-item">Traveler</div>
+            </div>
+          </div>
+          {/* <div className='i-description'>
+                        An aspiring MERN Stack Developer.I lost myself when I dive into the codes. I am a professional web designer and developer.No
+                    </div> */}
+          <a href="https://drive.google.com/uc?export=download&id=1nMf-DVHW2wxz-MxxJ5ZReIxUr3mM-xLW">
+            <button
+              type="button"
+              style={{
+                fontSize: "15px",
+                color: "whitesmoke",
+                fontWeight: 600,
+                margin: "20px",
+              }}
+              class="btn btn-info"
+            >
+              My Resume
+            </button>
+          </a>
+        </div>
+      </div>
+      <div className="i-right">
+        <img src={Me} alt="" className="i-img" />
+      </div>
     </div>
   );
 };
